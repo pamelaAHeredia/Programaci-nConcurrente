602a0c5c605601abd7fe879b62f1ab7b95fea65f
Pamela Heredia <pame.a.heredia@gmail.com>
2022-11-01 14:00:00 -0300

    ejercicio 7 - p5

--- p5-ADA/e7.cs
+++ p5-ADA/e7.cs
@@ -1,12 +1,42 @@
 /*Hay un sistema de reconocimiento de huellas dactilares de la policía que tiene 8 Servidores
 para realizar el reconocimiento, cada uno de ellos trabajando con una Base de Datos propia;
-a su vez hay un Especialista que utiliza indefinidamente. El sistema funciona de la siguiente
-manera: el Especialista toma una imagen de una huella (TEST) y se la envía a los servidores
+a su vez hay un Especialista que utiliza indefinidamente. 
+
+El sistema funciona de la siguiente manera: el Especialista toma una imagen de una huella (TEST) 
+y se la envía a los servidores
 para que cada uno de ellos le devuelva el código y el valor de similitud de la huella que más
 se asemeja a TEST en su BD; al final del procesamiento, el especialista debe conocer el
 código de la huella con mayor valor de similitud entre las devueltas por los 8 servidores.
+
 Cuando ha terminado de procesar una huella comienza nuevamente todo el ciclo. Nota:
 suponga que existe una función Buscar(test, código, valor) que utiliza cada Servidor donde
 recibe como parámetro de entrada la huella test, y devuelve como parámetros de salida el
 código y el valor de similitud de la huella más parecida a test en la BD correspondiente.
 Maximizar la concurrencia y no generar demora innecesaria.*/
+
+
+procedure Sistema is 
+    task type servidor is 
+        entry huella(h: in img); 
+    end servidor; 
+
+    servidores: array(1..8) of servidor; 
+
+    task especialista is 
+        entry resultadoServidor(c: in int, v: in int); 
+    end especialista
+
+    task body especialista
+        valorMax, codigoMax, total: int; 
+    begin   
+        while (true) loop  
+            for i:= 1..8 do loop
+
+            end 
+        end loop
+
+    end especialista; 
+
+begin 
+
+End Sistema; 
